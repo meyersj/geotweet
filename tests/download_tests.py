376fb389cd5f07b913dbf6d8cc6460878a7d21dc
Jeffrey Meyers <meyers2@pdx.edu>
2016-03-26 20:27:34 -0700

    add some basic tests

---
+++ tests/download_tests.py
@@ -0,0 +1,54 @@
+import unittest
+import os
+import sys
+
+root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(root)
+
+from geotweet.download import Downloader
+from geotweet.download import US_GEOFABRIK
+
+
+class DownloaderTests(unittest.TestCase):
+
+    def setUp(self):
+        self.dl = Downloader()
+        self.valid_state = "Rhode Island"
+        self.valid_url = US_GEOFABRIK.format("rhode-island")
+        self.invalid_url = US_GEOFABRIK.format("rhod-island")
+
+    def clear_tmp(self):
+        tmp = self.dl.get_filename(self.valid_url)
+        try:
+            os.remove(tmp)
+        except OSError:
+            pass
+
+    def test_url_valid(self):
+        state = "Rhode Island"
+        actual = self.dl.build_url(self.valid_state)
+        expected = self.valid_url 
+        error = "download url built incorrectly, actual: {0}, expected: {1}"
+        self.assertEqual(actual, expected, error.format(actual, expected))
+
+    def test_valid_download(self):
+        self.clear_tmp() 
+        res = self.dl._download(self.valid_url)
+        tmp = self.dl.get_filename(self.valid_url)
+        error = "File < {0} > was expected to be downloaded. does not exist"
+        self.assertTrue(os.path.isfile(tmp), error.format(tmp))
+        self.clear_tmp()
+    
+    def test_invalid_download(self):
+        self.clear_tmp() 
+        ioError = False
+        try:
+            res = self.dl._download(self.invalid_url)
+        except IOError as e:
+            ioError = True
+        error = "Invalid url {0} should have caused IOError"
+        self.assertTrue(ioError, error.format(self.invalid_url))
+        
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ tests/extract_tests.py
@@ -0,0 +1,40 @@
+import unittest
+import os
+import sys
+import subprocess
+
+root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(root)
+
+from geotweet.extract import Extractor
+
+
+class ExtractorTests(unittest.TestCase):
+
+    def setUp(self):
+        self.ex = Extractor()
+        self.source = os.path.join(root, 'data/rhode-island-latest.osm.pbf')
+        self.target = os.path.join(root, 'data/.tmp-rhode-island-latest.osm')
+
+    def clear_tmp(self, tmp):
+        try:
+            os.remove(tmp)
+        except OSError:
+            pass
+    
+    def test_osmosis(self):
+        ret = subprocess.call("osmosis -v", shell=True)
+        error = "Error checking version of osmosis with `osmosis -v`."
+        error += "\n\nIs it `osmosis` installed and on your path?"
+        self.assertTrue(ret == 0, error)
+
+    def test_url_valid(self):
+        self.clear_tmp(self.target)
+        res = self.ex._extract(self.source, self.target)
+        error = "Error: Extractor._extract returned False, Source: {0}, Target: {1}"
+        self.assertTrue(res, error.format(self.source, self.target))
+        self.clear_tmp(self.target)
+
+
+if __name__ == "__main__":
+    unittest.main()
