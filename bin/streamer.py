9347000229cca301aea18cd8f219c6b70098c655
Jeffrey Meyers <meyers2@pdx.edu>
2016-03-26 20:17:48 -0700

    add bin/stream which starts the twitter streaming api

---
+++ bin/streamer.py
@@ -0,0 +1,37 @@
+import os
+import sys
+
+from geotweet.log import logger
+from geotweet.stream import TwitterStream, LogTweetHandler
+
+
+LOG_DIR = os.getenv("GEOTWEET_STREAM_DIR", "/tmp/geotweet")
+INTERVAL = int(os.getenv("GEOTWEET_STREAM_LOG_INTERVAL", 5))
+# Bounding Boxes
+# [Lon,Lat SW corner, Lon,Lat NE corner]
+PORTLAND = ["-123.784828,44.683842", "-121.651703,46.188969"]
+US = ["-125.0011,24.9493", "-66.9326,49.5904"]
+LOCATIONS = US
+
+
+try:
+    os.mkdir(LOG_DIR)
+except:
+    pass
+
+
+def main():
+    log = os.path.join(LOG_DIR, 'twitter-stream.log')
+    
+    logger.info("Starting Twitter Streaming API")
+    logger.info("Streaming to output log: {0}".format(log))
+    logger.info("Log Interval (min): {0}".format(INTERVAL))
+    logger.info("Bounding Box: {0}".format(LOCATIONS))
+
+    handler = LogTweetHandler(log, interval=INTERVAL)
+    stream = TwitterStream()
+    stream.start(handler, locations=LOCATIONS)
+
+
+if __name__ == "__main__":
+    main()
