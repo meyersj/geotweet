48437a7c3ac44d6dea288d4cd64c3ddcd49bb0ae
Jeffrey Meyers <jeffrey.alan.meyers@gmail.com>
2016-03-28 18:50:47 -0400

    update runner script

--- bin/mapreduce_runner.sh
+++ bin/mapreduce_runner.sh
@@ -11,14 +11,15 @@ script="us-state-county-wordcount-v2.py"
 
 # ==========================
 
+logfile="twitter-stream.log.2016-03-26_13-13"
 
 # Local Job
-src="${DATADIR}/twitter-stream.log.2016-03-26_13-13"
+src="${DATADIR}/${logfile}"
 LOCAL_JOB_CMD="${interpreter} ${script} ${src}"
 
 # EMR Job
-src="s3://jeffrey.alan.meyers.geotweet/twitter-stream.log.2016-03-26_13-13"
-dst="s3://jeffrey.alan.meyers.geowordcount/geo-wordcount-output-test2/"
+src="s3://jeffrey.alan.meyers.geotweet/"
+dst="s3://jeffrey.alan.meyers.geowordcount/geo-wordcount-output-all/"
 EMR_JOB_CMD="${interpreter} ${script} ${src} -r emr --output-dir=${dst} --no-output"
 
 
