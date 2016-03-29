5f9eb8107d313e4df8f80f95c1d498eeb8df9981
Jeffrey Meyers <jeffrey.alan.meyers@gmail.com>
2016-03-29 16:29:36 -0700

    minor change to runner

--- bin/mapreduce_runner.sh
+++ bin/mapreduce_runner.sh
@@ -20,11 +20,12 @@ LOCAL_JOB_CMD="${interpreter} ${script} ${src}"
 
 
 # EMR Job
-src="s3://jeffrey.alan.meyers.geotweet/${logfile}"
-dst="s3://jeffrey.alan.meyers.bucket/geotweet/output/geo-wordcount-test/"
+now=`date +"%Y%m%d%H%M"`
+src="s3://jeffrey.alan.meyers.geotweet/"
+dst="s3://jeffrey.alan.meyers.bucket/geotweet/output/geo-wordcount-${now}"
 EMR_JOB_CMD="${interpreter} ${script} ${src} -r emr --output-dir=${dst} --no-output"
 
 
 # Run
-#${LOCAL_JOB_CMD}
-${EMR_JOB_CMD}
+${LOCAL_JOB_CMD}
+#${EMR_JOB_CMD}
