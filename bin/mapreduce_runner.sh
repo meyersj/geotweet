257a64c542c5426d787f909c722ad9a8df441f58
Jeffrey Meyers <jeffrey.alan.meyers@gmail.com>
2016-03-29 14:35:47 -0700

    mapreduce_runner.sh

--- bin/mapreduce_runner.sh
+++ bin/mapreduce_runner.sh
@@ -18,12 +18,13 @@ logfile="twitter-stream.log.2016-03-26_13-13"
 src="${DATADIR}/${logfile}"
 LOCAL_JOB_CMD="${interpreter} ${script} ${src}"
 
+
 # EMR Job
-src="s3://jeffrey.alan.meyers.geotweet/"
-dst="s3://jeffrey.alan.meyers.geowordcount/geo-wordcount-output-all-2/"
+src="s3://jeffrey.alan.meyers.geotweet/${logfile}"
+dst="s3://jeffrey.alan.meyers.bucket/geotweet/output/geo-wordcount-test/"
 EMR_JOB_CMD="${interpreter} ${script} ${src} -r emr --output-dir=${dst} --no-output"
 
 
 # Run
-${LOCAL_JOB_CMD}
-#${EMR_JOB_CMD}
+#${LOCAL_JOB_CMD}
+${EMR_JOB_CMD}
