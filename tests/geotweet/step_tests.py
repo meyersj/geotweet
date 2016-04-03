4eb22f0a8f496e186aec647d23c8c1b0c5c3869b
Jeffrey Meyers <meyers2@pdx.edu>
2016-04-02 20:58:38 -0700

    update tests with new structure

---
+++ tests/geotweet/step_tests.py
@@ -0,0 +1,60 @@
+import unittest
+import json
+import os
+from os.path import dirname
+import sys
+
+root = dirname(dirname(dirname(os.path.abspath(__file__))))
+sys.path.append(root)
+
+from geotweet.steps import GeoFilterStep, ExtractStep, LogStep
+
+
+TEST_STREAM = os.path.join(root, 'data/testdata/twitter-raw.log')
+
+
+class GeoFilterStepTests(unittest.TestCase):
+
+    def setUp(self):
+        self.step = GeoFilterStep()
+
+    def test_invalid_empty(self):
+        data = dict()
+        error = "Expected validate_geotweet to return False"
+        self.assertFalse(self.step.validate_geotweet(data), error)
+    
+    def test_invalid_none(self):
+        data = dict(coordinates=None, user=None)
+        error = "Expected validate_geotweet to return False"
+        self.assertFalse(self.step.validate_geotweet(data), error)
+
+    def test_invalid_geo(self):
+        data = dict(coordinates=None, user={'username':'jeff'})
+        error = "Expected validate_geotweet to return False"
+        self.assertFalse(self.step.validate_geotweet(data), error)
+
+    def test_valid(self):
+        data = dict(coordinates=[-122.5, 45.5], user={'username':'jeff'})
+        error = "Expected validate_geotweet to return True"
+        self.assertTrue(self.step.validate_geotweet(data), error)
+
+
+class ExtractStepTests(unittest.TestCase):
+
+    def setUp(self):
+        self.step = ExtractStep()
+        self.tweets = []
+        with open(TEST_STREAM, 'r') as f:
+            for tweet in f.read().splitlines():
+                self.tweets.append(json.loads(tweet))
+    
+    def test_extract(self):
+        for tweet in self.tweets:
+            if 'coordinates' in tweet and tweet['coordinates']:
+                data = self.step.process(tweet)
+                self.assertIsNotNone(data['lonlat'], 'Parsed tweet lonlat is None')
+                self.assertIsNotNone(data['user_id'], 'Parsed tweet user_id is None')
+
+
+if __name__ == "__main__":
+    unittest.main()
--- tests/geotweet/stream_tests.py
+++ tests/geotweet/stream_tests.py
@@ -8,16 +8,10 @@ root = dirname(dirname(dirname(os.path.abspath(__file__))))
 sys.path.append(root)
 
 from geotweet.stream import TwitterStream
-
+from geotweet.steps import ProcessStep
 
 TIMEOUT = 1
 BBOX = ["-125.0011,24.9493", "-66.9326,49.5904"]
-
-
-class MockHandler(object):
-
-    def handle(self, record):
-        pass
 
 
 class StreamTests(unittest.TestCase):
@@ -26,10 +20,10 @@ class StreamTests(unittest.TestCase):
         self.stream = TwitterStream()
         
     def test_stream_no_location(self):
-        self.stream.start(MockHandler(), timeout=TIMEOUT)
+        self.stream.start(ProcessStep(), timeout=TIMEOUT)
          
     def test_stream_location(self):
-        self.stream.start(MockHandler(), timeout=TIMEOUT, locations=BBOX)
+        self.stream.start(ProcessStep(), timeout=TIMEOUT, locations=BBOX)
 
 
 if __name__ == "__main__":
