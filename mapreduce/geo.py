312b4ce8367e929fa28363b4e2347b36c7aa69cf
Jeffrey Meyers <meyers2@pdx.edu>
2016-04-03 00:58:13 -0700

    remove utility classes from geo mapreduce job

--- mapreduce/geo.py
+++ mapreduce/geo.py
@@ -1,19 +1,21 @@
 import sys
 import os
+from os.path import dirname
 import re
-import hashlib
 import json
-import urllib2
-from urllib2 import HTTPError
 
 from mrjob.job import MRJob
 from mrjob.step import MRStep
 
 import Geohash
-import shapely
-from shapely.geometry import shape
-from shapely.geometry.point import Point
-from rtree import index
+
+root = dirname(dirname(os.path.abspath(__file__)))
+sys.path.append(root)
+
+from geotweet_utils.reader import FileReader
+from geotweet_utils.words import WordExtractor
+from geotweet_utils.lookup import CachedCountyLookup
+
 
 """
 https://en.wikipedia.org/wiki/Geohash
@@ -28,20 +30,6 @@ precision   width   height
 """
 GEOHASH_PRECISION = 7
 MIN_WORD_COUNT = 5      # ignore low occurences
-RTREE_LOCATION = '/tmp/geotweet-rtree-{0}'
-# Reference files to be downloaded from S3
-AWS_BUCKET = "https://s3-us-west-2.amazonaws.com/jeffrey.alan.meyers.bucket"
-STOPWORDS_LIST_URL = os.path.join(AWS_BUCKET, "geotweet/stopwords.txt")
-COUNTIES_GEOJSON_URL = os.path.join(AWS_BUCKET, "geotweet/us_counties.json")
-# use local files if these environment variables are set with a filepath
-try:
-    COUNTIES_GEOJSON = os.environ['COUNTIES_GEOJSON_LOCAL']
-except KeyError:
-    COUNTIES_GEOJSON = COUNTIES_GEOJSON_URL
-try:
-    STOPWORDS_LIST = os.environ['STOPWORDS_LIST_LOCAL']
-except KeyError:
-    STOPWORDS_LIST = STOPWORDS_LIST_URL
 
 
 class MRGeoWordCount(MRJob):
@@ -110,175 +98,6 @@ class MRGeoWordCount(MRJob):
             county = parts[2]
         # output results
         yield (parts[0], state, county), total
-
-
-class FileReader(object):
-    """ Read file from the local file system or remote url """
-
-    def is_url(self, src):
-        return src.startswith('http')
-
-    def is_valid_src(self, src):
-        return os.path.isfile(src) or self.is_url(src)
-
-    def read(self, src):
-        """ Download GeoJSON file of US counties from url (S3 bucket) """
-        geojson = None
-        if not self.is_valid_src(src):
-            error = "File < {0} > does not exists or does start with 'http'."
-            raise ValueError(error.format(src))
-        if not self.is_url(src):
-            return open(src, 'r').read().decode('latin-1').encode('utf-8')
-        response = urllib2.urlopen(src)
-        return response.read().decode('latin-1').encode('utf-8')
-
-
-class WordExtractor(FileReader):
-    """
-    Extract words from a tweet.
-
-    If a provided `src` keyword param references a local file or remote resource
-    containing list of stop words, the will be download and used to exclude
-    extracted words
-    
-    """
-    def __init__(self, src=STOPWORDS_LIST):
-        self.sub_all = re.compile("""[#.!?,"(){}[\]|]|&amp;""")
-        self.sub_ends = re.compile("""^[@\\\/~]*|[\\\/:~]*$""")
-        self.stopwords = {}
-        if src:
-            if not self.is_valid_src(src):
-                error = "Arg src=< {0} > is invalid."
-                error += " Must be existing file or url that starts with 'http'"
-                raise ValueError(error.format(src))
-            words = self.read(src)
-            for word in words.splitlines():
-                    self.stopwords[word] = ""
-
-    def clean_unicode(self, line):
-        chars = [char for char in line.lower()]
-        only_ascii = lambda char: char if ord(char) < 128 else ''
-        return str(''.join(filter(only_ascii, chars)))
-
-    def clean_punctuation(self, word):
-        return self.sub_ends.sub('', self.sub_all.sub('', word))
-
-    def run(self, line):
-        """
-        Extract words from tweet
-
-        1. Remove non-ascii characters
-        2. Split line into individual words
-        3. Clean up puncuation characters
-        
-        """
-        words = []
-        for word in self.clean_unicode(line.lower()).split():
-            if word.startswith('http'):
-                continue
-            cleaned = self.clean_punctuation(word)
-            if len(cleaned) > 1 and cleaned not in self.stopwords:
-                words.append(cleaned)
-        return words
-
-
-class SpatialLookup(FileReader):
-    """ Create a indexed spatial lookup of a geojson file """
-    
-    idx = None
-
-    def __init__(self, src=None):
-        if src:
-            if not self.is_valid_src(src):
-                error = "Arg src=< {0} > is invalid."
-                error += " Must be existing file or url that starts with 'http'"
-                raise ValueError(error.format(src))
-            # location of index based on hash on input src name
-            location = self.get_location(src)
-            if not self._exists(location):
-                # index does not create so fetch data and build it
-                self._build(src, location)
-            else:
-                # index already exists
-                self.idx = index.Rtree(location)
-    
-    def get_location(self, src):
-        digest = self.digest(src)
-        if not digest:
-            return None
-        return RTREE_LOCATION.format(digest)
-
-    def digest(self, src):
-        if not src or type(src) != str:
-            return None
-        m = hashlib.md5()
-        if self.is_url(src):
-            m.update(src)
-        else:
-            m.update(os.path.abspath(src))
-        return m.hexdigest()
-
-    def _exists(self, location):
-        dat = location + ".dat"
-        idx = location + ".idx"
-        if os.path.isfile(dat) and os.path.isfile(idx):
-            return True
-        return False
-
-    def get_object(self, point):
-        """ lookup object based on point as [longitude, latitude] """
-        # first search bounding boxes
-        # idx.intersection method modifies input if it is a list
-        try:
-            tmp = tuple(point)
-        except TypeError:
-            return None
-        # point must be in the form (minx, miny, maxx, maxy) or (x, y)
-        if len(tmp) not in [2, 4]:
-            return None
-        for bbox_match in self.idx.intersection(tmp, objects=True):
-            # check actual geometry
-            record = bbox_match.object
-            if record['geometry'].intersects(Point(tmp)):
-                return record['properties']
-        return None
-   
-    def _build(self, src, location):
-        """ Build a RTree index to disk using bounding box of each feature """
-        geojson = json.loads(self.read(src))
-        def generate():
-            for i, feature in enumerate(geojson['features']):
-                feature['geometry'] = shape(feature['geometry'])
-                yield i, feature['geometry'].bounds, feature
-        if geojson:
-            # create index, flush to disk which disables access then re-enable access
-            self.idx = index.Rtree(location, generate())
-            self.idx.close()
-            self.idx = index.Rtree(location)
-
-
-class CachedCountyLookup(SpatialLookup):
-    """ Dowload counties geojson, build index and provide lookup functionality """
-
-    geohash_cache = {}
-
-    def __init__(self, src=COUNTIES_GEOJSON):
-        super(CachedCountyLookup, self).__init__(src=src)
-
-    def get(self, geohash):
-        """ lookup state and county based on geohash of coordinates from tweet """
-        if geohash in self.geohash_cache:
-            # cache hit on geohash
-            return self.geohash_cache[geohash]
-        # cache miss on geohash
-        state = county = None
-        coord = Geohash.decode(geohash)
-        prop = self.get_object([float(coord[1]), float(coord[0])])
-        if prop:
-            state = prop['STATE']
-            county = prop['COUNTY']
-        self.geohash_cache[geohash] = (state, county)
-        return state, county
 
 
 if __name__ == '__main__':
