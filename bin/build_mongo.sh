eabdd179918deb4c068ee6014140f8f1a340aaf0
Jeffrey Meyers <meyers2@pdx.edu>
2016-04-04 13:30:47 -0700

    fix flag from mongo to db

--- bin/build_mongo.sh
+++ bin/build_mongo.sh
@@ -10,7 +10,7 @@ db=geotweet
 # clear geotweet database
 drop_db ${db}
 
-./geoloader osm --mongo ${db} --states ../data/states/states.txt osm 
-./geoloader geojson --mongo ${db} ../data/geo/us_metro_areas.geojson metro
-./geoloader geojson --mongo ${db} ../data/geo/us_states.geojson states
-./geoloader geojson --mongo ${db} ../data/geo/us_counties.geojson counties
+./geoloader osm --db ${db} --states ../data/states/states.txt osm 
+./geoloader geojson --db ${db} ../data/geo/us_metro_areas.geojson metro
+./geoloader geojson --db ${db} ../data/geo/us_states.geojson states
+./geoloader geojson --db ${db} ../data/geo/us_counties.geojson counties
