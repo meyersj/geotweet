e60ca38a8d990892521a79c1381504e9c650b733
Jeffrey Meyers <meyers2@pdx.edu>
2016-04-04 17:12:30 -0700

    update setup.py to include some data files

--- setup.py
+++ setup.py
@@ -6,16 +6,24 @@ except ImportError:
 
 setup(
     name='geotweet',
-    version='0.1.3',
+    version='0.1.6',
     description='Fetch geographic tweets from Twitter Streaming API',
     author='Jeffrey Alan Meyers',
     author_email='jeffrey.alan.meyers@gmail.com',
     url='https://github.com/meyersj/geotweet',
-    packages=['geotweet'],
-    scripts=['bin/geotweet'],
+    packages=['geotweet', 'geotweet.twitter', 'geotweet.mongo', 'geotweet.geo'],
+    scripts=['bin/geotweet', 'bin/geoloader'],
+    include_package_data=True,
     install_requires=[
         'argparse',
         'boto3',
-        'python-twitter'
+        'python-twitter',
+        'requests',
+        'mrjob',
+        'pymongo',
+        'Geohash',
+        'Shapely',
+        'Rtree',
+        'lxml'
     ]
 )
