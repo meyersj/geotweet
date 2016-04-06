8a1e5f90eb34812500c8f7003585f0c0a1836523
Jeffrey Meyers <meyers2@pdx.edu>
2016-04-06 12:29:07 -0700

    update mapreduce bootstrap script to use geotweet pip package

--- bin/bootstrap_mapreduce.sh
+++ bin/bootstrap_mapreduce.sh
@@ -3,15 +3,15 @@
 
 # extra commands required when starting from fresh Centos Box
 # When running on EMR these commands are not required
+# -------------------------------------------------------------
 #yum install -y epel-release
 #curl "https://bootstrap.pypa.io/get-pip.py" -o "/tmp/get-pip.py"
 #python /tmp/get-pip.py
-
+# -------------------------------------------------------------
 
 # system packages
 yum update -y
-yum install -y gcc-c++ geos geos-devel python-devel libxml2-devel  libxslt-devel
-
+yum install -y gcc-c++ geos geos-devel python-devel
 
 # install libspatialindex for Rtree
 cd /opt
@@ -22,9 +22,5 @@ cd spatialindex-src-1.8.5
 echo /usr/local/lib >> /etc/ld.so.conf
 ldconfig
 
-
-# python packages
-pip install Geohash==1.0
-pip install Shapely==1.5.14
-pip install Rtree==0.8.2
-pip install pymongo==3.2.2
+# dependencies for MapReduce jobs
+pip install geotweet
