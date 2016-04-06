767818d6f4dde49b377445355b5164f4ee6902a7
Jeffrey Meyers <meyers2@pdx.edu>
2016-04-06 12:28:48 -0700

    update mapreduce bootstrap script to use geotweet pip package

--- bin/mapreduce_runner.sh
+++ bin/mapreduce_runner.sh
@@ -1,31 +1,32 @@
 #!/bin/bash
 
 SCRIPTDIR=`dirname $(readlink -f $0)`
-JOBDIR=`readlink -f ${SCRIPTDIR}/../mapreduce`
-DATADIR=`readlink -f ${SCRIPTDIR}/../data/mapreduce`
+JOBDIR=`readlink -f ${SCRIPTDIR}/../geotweet/mapreduce`
+DATADIR=`readlink -f ${SCRIPTDIR}/../geotweet/data/mapreduce`
 
 
 # === MR Job Parameters ===
 
-interpreter=python
-script="${JOBDIR}/geo.py"
+interpreter=../env/bin/python
+script="${JOBDIR}/state_county_wordcount.py"
 
 # ==========================
 
 
 # Local Job
-logfile="twitter-stream.log.2016-03-26_13-13"
+logfile="twitter-stream.log.2016-03-27_01-53"
 src="${DATADIR}/${logfile}"
 LOCAL_JOB_CMD="${interpreter} ${script} ${src}"
 
 
 # EMR Job
 now=`date +"%Y%m%d%H%M"`
-src="s3://jeffrey.alan.meyers.geotweet/"
-dst="s3://jeffrey.alan.meyers.bucket/geotweet/output/geo-wordcount-${now}"
-EMR_JOB_CMD="${interpreter} ${script} ${src} -r emr --output-dir=${dst} --no-output"
-
+src="s3://jeffrey.alan.meyers.geotweet-test/"
+dst="--output-dir s3://jeffrey.alan.meyers.bucket/geotweet/output/geo-wordcount-${now}"
+conf="--conf-path ~/scratch/mrjob.conf"
+EMR_JOB_CMD="${interpreter} ${script} -r emr ${src} ${dst} --no-output"
+EMR_JOB_CMD="${interpreter} ${script} -r emr ${conf} ${src} ${dst} --no-output"
 
 # Run
-${LOCAL_JOB_CMD}
-#${EMR_JOB_CMD}
+#${LOCAL_JOB_CMD}
+${EMR_JOB_CMD}
