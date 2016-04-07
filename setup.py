d3fe8d3641745cbcca2a968178f98d7014e6368f
Jeffrey Meyers <meyers2@pdx.edu>
2016-04-06 19:31:09 -0700

    increment version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='geotweet',
-    version='0.1.18',
+    version='0.1.20',
     description='Fetch geographic tweets from Twitter Streaming API',
     author='Jeffrey Alan Meyers',
     author_email='jeffrey.alan.meyers@gmail.com',
