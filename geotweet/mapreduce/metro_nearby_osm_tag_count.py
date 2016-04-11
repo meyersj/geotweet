061c1c9ddcb829a36e5dd27c2b49c004ba6670e1
Jeffrey Meyers <meyers2@pdx.edu>
2016-04-10 23:16:27 -0700

    working on getting implementation working on emr

---
+++ geotweet/mapreduce/metro_nearby_osm_tag_count.py
@@ -0,0 +1,181 @@
+import sys
+import os
+from os.path import dirname
+import re
+import json
+import logging
+import resource
+
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+from mrjob.protocol import JSONValueProtocol, JSONProtocol
+import Geohash
+
+try:
+    # when running on EMR a geotweet package will be loaded onto PYTHON PATH
+    from geotweet.mapreduce.utils.lookup import CachedMetroLookup, CachedLookup
+    from geotweet.mapreduce.utils.proj import project, rproject
+    from geotweet.geomongo.mongo import MongoGeo
+except ImportError:
+    # running locally
+    from utils.lookup import CachedMetroLookup, CachedLookup
+    from utils.proj import project, rproject
+    parent = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    sys.path.insert(0, parent) 
+    from geomongo.mongo import MongoGeo
+
+
+# must log to stderr when running on EMR or job will fail
+logging.basicConfig(stream=sys.stderr, level=logging.INFO)
+
+
+DB = "geotweet"
+COLLECTION = "metro_osm"
+MIN_WORD_COUNT = 2
+METERS_PER_MILE = 1609
+METRO_DISTANCE = 50 * METERS_PER_MILE
+POI_DISTANCE = 100
+MONGO_TIMEOUT = 30 * 1000
+POI_TAGS = ["amenity", "builing", "shop", "office", "tourism"]
+"""
+https://en.wikipedia.org/wiki/Geohash
+
+geohash
+precision   width   height
+4           39.1km  19.5km
+5           4.9km   4.9km
+6           1.2km   609.4m
+7           152.9m  152.4m
+8           38.2m   19m
+"""
+GEOHASH_PRECISION = 7 
+
+
+class MRMetroNearbyOSMTagCount(MRJob):
+    """ """
+    
+    INPUT_PROTOCOL = JSONValueProtocol
+    INTERNAL_PROTOCOL = JSONProtocol
+    OUTPUT_PROTOCOL = JSONProtocol
+    SORT_VALUES = True
+    
+    def steps(self):
+        return [
+            # lookup metro area for each geotweet and osm POI
+            # and emit to same reducer to perform POI lookup
+            MRStep(
+                mapper_init=self.mapper_init_metro,
+                mapper=self.mapper_metro,
+                reducer=self.reducer_metro
+            ),
+            # lookup nearby POIs around geotweet
+            # emit each OSM tag value that we are interested in 
+            MRStep(
+                reducer=self.reducer_tag_count
+            ),
+            # count POI tags for each metro area 
+            MRStep(
+                reducer_init=self.reducer_init_mongo,
+                reducer=self.reducer_mongo
+            )
+        ]
+    
+    def mapper_init_metro(self):
+        """ build local spatial index of US metro areas """
+        self.lookup = CachedMetroLookup(precision=GEOHASH_PRECISION)
+        self.hr = 0
+
+    def mapper_metro(self, _, line):
+        data = line
+        #data = json.loads(line)
+        if 'tags' in data:
+            tag = 1
+            # initalize data for POI
+            lonlat = data['coordinates']
+            payload = (lonlat, data['tags'])
+        elif 'user_id' in data:
+            tag = 2
+            # ignore HR geo-tweets for job postings
+            expr = "|".join(["(job)", "(hiring)", "(career)"])
+            if data['description'] and re.findall(expr, data['description']):
+                self.hr += 1
+                return
+            # initialize data for getweet
+            lonlat = data['lonlat']
+            payload = lonlat
+        metro = self.lookup.get(lonlat, METRO_DISTANCE)
+        if not metro:
+            return
+        yield metro, (tag, payload)
+   
+
+    def reducer_metro(self, metro, values):
+        """
+        Output tags of POI locations nearby tweet locations
+
+        Values will be sorted coming into reducer.
+        First element in each value tuple will be either 1 (osm POI) or 2 (geotweet).
+        Build a spatial index with POI records.
+        For each tweet lookup nearby POI, and emit tag values for predefined tags.
+        
+        """
+        lookup = CachedLookup(precision=GEOHASH_PRECISION)
+        #for value in values:
+        i = 0
+        while True:
+            try:
+                value = values.next()
+            except StopIteration:
+                break
+            except Exception as e:
+                logging.error(e)
+                logging.error(metro)
+                logging.error(value)
+                logging.error("Unexpected excpetion in reducer_metro")
+                raise e
+            i += 1
+            mr_tag, data = value
+            if mr_tag == 1:
+                # OSM POI node, add to index
+                lonlat, tags = data
+                lookup.insert(i, dict(
+                    geometry=dict(type='Point', coordinates=project(lonlat)),
+                    properties=dict(tags=tags)
+                ))
+            else:
+                # geotweet, lookup nearest POI from index
+                lonlat = data
+                tags = []
+                kwargs = dict(buffer_size=POI_DISTANCE, multiple=True)
+                # lookup nearby POI from Rtree index (caching results)
+                # for any tags we care about emit the tags value and 1
+                for poi in lookup.get(lonlat, **kwargs):
+                    for tag in POI_TAGS:
+                        if tag in poi['tags']:
+                            tags.append(poi['tags'][tag])
+                for tag in set(tags):
+                    yield (metro, tag), 1
+
+    def reducer_tag_count(self, key, values):
+        total = sum(values)
+        metro, tag = key
+        yield metro, (total, tag)
+
+    def reducer_init_mongo(self):
+        self.mongo = MongoGeo(db=DB, collection=COLLECTION, timeout=MONGO_TIMEOUT)
+
+    def reducer_mongo(self, metro, values):
+        records = []
+        for record in values:
+            total, tag = record
+            records.append(dict(
+                metro_area=metro,
+                tag=tag,
+                count=total
+            ))
+            yield (metro, total), tag
+        self.mongo.insert_many(records)
+
+
+if __name__ == '__main__':
+    MRMetroNearbyOSMTagCount.run()
