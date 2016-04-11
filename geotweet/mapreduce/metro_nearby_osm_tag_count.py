e4b5062d155a8d14c40e0df5841787cd199f3931
Jeffrey Meyers <meyers2@pdx.edu>
2016-04-11 15:35:52 -0700

    almost working .. on emr

--- geotweet/mapreduce/metro_nearby_osm_tag_count.py
+++ geotweet/mapreduce/metro_nearby_osm_tag_count.py
@@ -2,14 +2,14 @@ import sys
 import os
 from os.path import dirname
 import re
-import json
 import logging
 import resource
 
 from mrjob.job import MRJob
 from mrjob.step import MRStep
-from mrjob.protocol import JSONValueProtocol, JSONProtocol
+from mrjob.protocol import RawValueProtocol, RawProtocol, JSONProtocol
 import Geohash
+import ujson
 
 try:
     # when running on EMR a geotweet package will be loaded onto PYTHON PATH
@@ -25,8 +25,14 @@ except ImportError:
     from geomongo.mongo import MongoGeo
 
 
+#logging.basicConfig(stream=sys.stderr, level=logging.INFO)
 # must log to stderr when running on EMR or job will fail
-logging.basicConfig(stream=sys.stderr, level=logging.INFO)
+logger = logging.getLogger(__file__)
+formatter = logging.Formatter('[%(levelname)s] %(message)s')
+handler = logging.StreamHandler(stream=sys.stderr)
+handler.setFormatter(formatter)
+handler.setLevel(logging.INFO)
+logger.addHandler(handler)
 
 
 DB = "geotweet"
@@ -51,12 +57,20 @@ precision   width   height
 GEOHASH_PRECISION = 7 
 
 
+def dumps(src):
+    return ujson.dumps(src, ensure_ascii=False)
+
+
+def loads(src):
+    return ujson.loads(src)
+
+
 class MRMetroNearbyOSMTagCount(MRJob):
     """ """
     
-    INPUT_PROTOCOL = JSONValueProtocol
-    INTERNAL_PROTOCOL = JSONProtocol
-    OUTPUT_PROTOCOL = JSONProtocol
+    INPUT_PROTOCOL = RawValueProtocol
+    INTERNAL_PROTOCOL = RawProtocol
+    OUTPUT_PROTOCOL = RawProtocol
     SORT_VALUES = True
     
     def steps(self):
@@ -86,13 +100,12 @@ class MRMetroNearbyOSMTagCount(MRJob):
         self.hr = 0
 
     def mapper_metro(self, _, line):
-        data = line
-        #data = json.loads(line)
+        data = loads(line)
         if 'tags' in data:
             tag = 1
             # initalize data for POI
             lonlat = data['coordinates']
-            payload = (lonlat, data['tags'])
+            payload = (tag, lonlat, data['tags'])
         elif 'user_id' in data:
             tag = 2
             # ignore HR geo-tweets for job postings
@@ -102,12 +115,11 @@ class MRMetroNearbyOSMTagCount(MRJob):
                 return
             # initialize data for getweet
             lonlat = data['lonlat']
-            payload = lonlat
+            payload = (tag, lonlat, None)
         metro = self.lookup.get(lonlat, METRO_DISTANCE)
         if not metro:
             return
-        yield metro, (tag, payload)
-   
+        yield metro.encode('utf-8'), dumps(payload)
 
     def reducer_metro(self, metro, values):
         """
@@ -119,32 +131,33 @@ class MRMetroNearbyOSMTagCount(MRJob):
         For each tweet lookup nearby POI, and emit tag values for predefined tags.
         
         """
-        lookup = CachedLookup(precision=GEOHASH_PRECISION)
-        #for value in values:
-        i = 0
-        while True:
-            try:
-                value = values.next()
-            except StopIteration:
-                break
-            except Exception as e:
-                logging.error(e)
-                logging.error(metro)
-                logging.error(value)
-                logging.error("Unexpected excpetion in reducer_metro")
-                raise e
-            i += 1
-            mr_tag, data = value
+        lookup = CachedLookup(precision=GEOHASH_PRECISION+1)
+        for i, value in enumerate(values):
+            if not value:
+                continue
+            #logger.error(value)
+            #try:
+            #logger.error(type(value))
+            value = loads(value)
+            #logger.error(value)
+            #except ValueError as e:
+            #    logger.error(e.__class__.__name__)
+            #    logger.error(str(e))
+            #    logger.error(metro)
+            #    logger.error(i)
+            #    logger.error(value)
+            #    continue
+            mr_tag, lonlat, data = value
             if mr_tag == 1:
                 # OSM POI node, add to index
-                lonlat, tags = data
                 lookup.insert(i, dict(
                     geometry=dict(type='Point', coordinates=project(lonlat)),
-                    properties=dict(tags=tags)
+                    properties=dict(tags=data)
                 ))
             else:
                 # geotweet, lookup nearest POI from index
-                lonlat = data
+                if not lookup.data_store:
+                    return
                 tags = []
                 kwargs = dict(buffer_size=POI_DISTANCE, multiple=True)
                 # lookup nearby POI from Rtree index (caching results)
@@ -153,28 +166,60 @@ class MRMetroNearbyOSMTagCount(MRJob):
                     for tag in POI_TAGS:
                         if tag in poi['tags']:
                             tags.append(poi['tags'][tag])
-                for tag in set(tags):
-                    yield (metro, tag), 1
+                for osm_tag in set(tags):
+                    if not osm_tag:
+                        continue
+                    key = (metro, osm_tag.encode('utf-8'))
+                    yield dumps(key), dumps(1)
 
     def reducer_tag_count(self, key, values):
-        total = sum(values)
-        metro, tag = key
-        yield metro, (total, tag)
+        total = 0
+        #logger.error(key)
+        if not key:
+            return
+        for value in values:
+            if not value or value == '':
+                continue
+            #logging.error(value)
+            #logging.error('accept')
+            try:
+                value = loads(value)
+                total += int(value)
+            except ValueError as e:
+                #logger.info(e)
+                #logger.info(value)
+                continue
+        #logger.error(total)
+        if total == 0:
+            return
+        metro, tag = ujson.loads(key)
+        yield metro.encode('utf-8'), dumps((total, tag))
 
     def reducer_init_mongo(self):
-        self.mongo = MongoGeo(db=DB, collection=COLLECTION, timeout=MONGO_TIMEOUT)
-
+        pass
+        #self.mongo = MongoGeo(db=DB, collection=COLLECTION, timeout=MONGO_TIMEOUT)
+    
     def reducer_mongo(self, metro, values):
+        #logger.info(metro)
+        if not metro:
+            return
         records = []
         for record in values:
-            total, tag = record
+            if not record:
+                continue
+            #logger.info(record)
+            try:
+                total, tag = ujson.loads(record)
+            except ValueError as e:
+                #logger.info(e)
+                continue
             records.append(dict(
                 metro_area=metro,
                 tag=tag,
                 count=total
             ))
-            yield (metro, total), tag
-        self.mongo.insert_many(records)
+            yield dumps((metro, total)), tag.encode('utf-8')
+        #self.mongo.insert_many(records)
 
 
 if __name__ == '__main__':
