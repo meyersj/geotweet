511c78263476854cb2ca7c13267b4ccf4ecedae5
Jeffrey Meyers <meyers2@pdx.edu>
2016-04-10 17:29:45 -0700

    store data in lookup_tests

--- geotweet/tests/unit/mapreduce/utils/lookup_tests.py
+++ geotweet/tests/unit/mapreduce/utils/lookup_tests.py
@@ -41,14 +41,16 @@ def init_polygon_1_index():
     location = SpatialLookup()
     idx = index.Rtree()
     polygon = location._build_obj(POLYGON_1)
-    idx.insert(1, polygon['geometry'].bounds, obj=polygon)
+    location.data_store[1] = polygon
+    idx.insert(1, polygon['geometry'].bounds)
     location.idx = idx
     return location
 
 def init_polygon_2_index():
     location = init_polygon_1_index()
     polygon = location._build_obj(POLYGON_2)
-    location.idx.insert(2, polygon['geometry'].bounds, obj=polygon)
+    location.data_store[2] = polygon
+    location.idx.insert(2, polygon['geometry'].bounds)
     return location
 
 
@@ -117,7 +119,6 @@ class GetObjectOrder(unittest.TestCase):
         error = "get_object failed to return object with id=polygon1: Actual < {0} >"
         self.assertEqual('polygon1', found['id'], error.format(found['id']))
    
-
 
 if __name__ == "__main__":
     unittest.main()
