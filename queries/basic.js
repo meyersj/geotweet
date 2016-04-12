0bd823a5765f9aacb0ed8f4d55fd3761f66c940c
Jeffrey Meyers <meyers2@pdx.edu>
2016-04-11 18:57:52 -0700

    update basic queries

--- queries/basic.js
+++ queries/basic.js
@@ -1,37 +1,22 @@
-// return Documents from 'metro' collection that
-//  have a geojson object stored at key=geometry
-//  be within 30000 meters from Portland
-db.metro.find({
-    geometry:{
-        $near:{
-            $geometry: {type:"Point", coordinates:[-122.5, 45.5]},
-            $maxDistance: 30000  // meters
-        }
-    }
-})
-
-
-// Collection wide command
-db.runCommand({
-    geoNear:"metro",
-    near: {type:"Point", coordinates:[-122.5, 45.5]},
-    spherical: true,
-    limit: 2
-})
-
-
 // List out all Metro Areas
-db.geotweet.distinct("metro_area")
-
+db.metro_word.distinct("metro_area")
 
 // Get top word occurences for Portland
-db.geotweet.find({
+db.metro_word.find({
     metro_area: "Portland, OR--WA"
 }).sort({count:-1})
 
-
-// Top occurences
-db.geotweet.aggregate({
+db.metro_word.aggregate({
     $sort: { count:-1 }
 })
 
+// metro_nearby_osm_tag_count
+db.metro_osm.distinct("metro_area")
+
+db.metro_osm.find({
+    metro_area: "Portland, OR--WA"
+}).sort({count:-1})
+
+db.metro_osm.aggregate({
+    $sort: { count:-1 }
+})
